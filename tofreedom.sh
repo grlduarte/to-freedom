968df4803ee172b7f5891dbf996107ad5966685e
guites <71985299+guites@users.noreply.github.com>
2022-03-31 08:42:40 -0300

    Add bsd utils support

--- tofreedom.sh
+++ tofreedom.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 
-# current_day=$(date +'%Y%m%d')
-# debian version (GNU)
-# current_day=$(date -d '20220330 17:00:00' +'%Y%m%d')
-# osx version
-current_day=$(date -j -f "%Y%m%d %T" "20220330 17:00:00" +'%Y%m%d')
-
+# checks whether the system is running gnu or bsd utils
 has_gnu_date() {
   date -d '20220330 17:00:00' +'%Y%m%d' >/dev/null 2>&1
   return $?
@@ -22,22 +17,47 @@ date_from_string() {
   echo $seconds_from_epoch
 }
 
+# $1 is date in seconds elapsed since January 1, 1970 (midnight UTC/GMT), not counting leap seconds
+# $2 is the desired output format ex. "%Y%m%d %H:%M"
+epoch_to_date() {
+  if has_gnu_date; then
+    echo "gnu's not unix" # todo: test correct gnu syntax
+  else
+    output_date=$(date -j -f "%s" "$1" "$2")
+  fi
+  echo $output_date
+}
+
+# uses awk to perform floating point arithmetics
 # source https://unix.stackexchange.com/a/40897
 calc(){ awk "BEGIN { print $*}"; }
 
+# verifies whether user input is in HH:MM format
+# $1 is user input
+check_input_format() {
+  if ! [[ "$1" =~ ^[0-9]{2}:[0-9]{2}$ ]]; then
+    echo "Digite os horários no formato HH:MM!"
+    exit 1
+  fi
+}
+
+current_day=$(date +'%Y%m%d')
+
 echo "Que horas você começou a trabalhar?"
-#read start_time
-start_time="08:00"
+read start_time
+check_input_format $start_time
 start_time_formatted=$(date_from_string "$current_day $start_time" +'%H:%M %d/%m/%Y')
 echo " - Inicio do turno às "$start_time_formatted
+
 echo "Que horas você parou para o intervalo do almoço?"
-#read lunch_start
-lunch_start="12:00"
+read lunch_start
+check_input_format $lunch_start
 lunch_start_formatted=$(date_from_string "$current_day $lunch_start" +'%H:%M %d/%m/%Y')
 echo " - Saída para o almoço às "$lunch_start_formatted
+
 echo "Que horas você voltou do intervalo do almoço?"
-#read lunch_end
-lunch_end="13:00"
+read lunch_end
+check_input_format $lunch_end
 lunch_end_formatted=$(date_from_string "$current_day $lunch_end" +'%H:%M %d/%m/%Y')
 echo " - Retorno do almoço às "$lunch_end_formatted
 
@@ -47,6 +67,7 @@ lunch_end_epoch=$(date_from_string "$current_day $lunch_end" "+%s")
 
 morning_worked_seconds=$((lunch_start_epoch-start_time_epoch))
 morning_worked_hours=$(calc "$morning_worked_seconds/60/60")
+
 # check if $morning_worked_hours has decimal values
 grep "\." <<< "$morning_worked_hours" &> /dev/null
 if [ $? == 0 ]; then
@@ -62,22 +83,18 @@ fi
 total_hours=8
 remaining_second_shift_hours=$(calc "$total_hours-$morning_worked_hours")
 
-
-grep "\." <<< "$remaining_afternoon_hours" &> /dev/null
+# check if remaining hours in the second shift has decimal values and formats it
+grep "\." <<< "$remaining_second_shift_hours" &> /dev/null
 if [ $? == 0 ]; then
-    remaining_hours_int=${remaining_hours%.*}
-    remaining_hours_dec=${remaining_hours#*.}
+    remaining_hours_int=${remaining_second_shift_hours%.*}
+    remaining_hours_dec=${remaining_second_shift_hours#*.}
     remaining_minutes=$((remaining_hours_dec*60))
-    echo "você está a "$remaining_hours_int":"${remaining_minutes:0:2}" horas da liberdade!"
+    echo "Você precisa trabalhar "$remaining_hours_int":"${remaining_minutes:0:2}" horas no turno da tarde!"
 else
-    echo "você está a $remaining_hours:00 horas da liberdade!"
+    echo "você precisa trabalhar $remaining_second_shift_hours:00 horas no turno da tarde!"
 fi
-remaining_seconds=$(calc $remaining_hours*60*60)
+remaining_seconds=$(calc $remaining_second_shift_hours*60*60)
 
-if has_gnu_date; then
-  echo "Seu turno termina às "$(date -d '+'$remaining_seconds' seconds' +'%d %m %Y %H:%M')
-else
-  echo "Seu turno termino às "$(date -v+"$remaining_seconds"S -j +'%H:%M %d/%m/%Y')
-fi
-
-# TODO: levar em conta o lunch_end na hora do cálculo
+logoff_time_epoch=$((lunch_end_epoch+remaining_seconds))
+logoff_date=$(epoch_to_date $logoff_time_epoch +'%H:%M %d/%m/%Y')
+echo "Seu turno termina às $logoff_date"
